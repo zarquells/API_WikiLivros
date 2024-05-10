6f3d21db8fb70f4fa1e3f8922de70b0c364c7b3d
Érica Lelis <116844017+zarquells@users.noreply.github.com>
2024-05-10 11:49:34 -0300

    Construção da Rota para a Página Book
    Detalhes
    Utilizei API local e falhei em colocar imagens na API portanto estamos somente com os textos e modifiquei as pastas de .env para isso.

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const book = () => {
+import "./book.css";
+
+const URL = import.meta.env.VITE_SEARCHID;
+
+const Book = () => {
+  const { _id } = useParams();
+  const [book, setBook] = useState(null);
+
+  const getBook    = async (URL) => {
+    const response = await fetch(URL);
+    const data     = await response.json();
+    console.log(data);
+    setBook(data);
+  };
+
+  useEffect(() => {
+    const bookURL = `${URL}${_id}`;
+    getBook(bookURL);
+    
+  }, []);
+
   return (
-    <div>book</div>
-  )
-}
+  <div className="movie-page">
+    {book && (
+      <p>{book.name}</p>
+    )}
+  </div>
+  );
+};
 
-export default book
+export default Book;
