cff2eb8454970332b328e109694af571d6ca9343
Érica Lelis <116844017+zarquells@users.noreply.github.com>
2024-05-13 09:01:14 -0300

    A última visita do Create
    Atila pediu para que não fizéssemos um CRUD perfeito....

--- src/pages/create.jsx
+++ src/pages/create.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
-const create = () => {
+import Check from '../components/check';
+
+import './create.css';
+
+// const URL = import.meta.env.VITE_URL;
+
+const Create = () => {
+
+  //Enviar novo livro para a API
+  const [NovoLivro, setNovoLivro] = useState({
+    name: '',
+    author: '',
+    description: '',
+    year: 0
+})
+
+  // Pré-visualização da imagem inserida
+  const [img, setImg] = useState();
+  function exibirImg(e) {
+     console.log(e.target.files);
+      setImg(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
-    <div>create</div>
-  )
+    <div className='Create'>
+        <main className='Formulario'>
+          {/* Input de imagem */}
+            <div className='quadrado'>
+              <img src={img} alt=""/>
+              <input id="arquivo" type='file' className="filetype" onChange={exibirImg} multiple accept="image/*"></input>
+            </div>
+          <div className='form-cont'>
+          
+          {/*Input de formulário*/}
+          <div className='form-info'>
+            <input type='text'   placeholder='Adicionar um título'></input>      
+            <input type='text'   placeholder='Nome do Autor'></input>    
+            <input type='number' placeholder='Ano' min='0' max='2025' id='ano'></input>          
+            <textarea type='text'placeholder='Insira a sinopse do livro...' id='descricao'></textarea>
+          </div>
+          <input type='submit'   placeholder='Adicionar livro' id='enviar'></input>
+          </div>
+        </main>
+
+        <Check/>
+    </div>
+
+  );
 }
 
-export default create
+export default Create;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,31 +1,61 @@
-import { useState, useEffect } from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
-//não esquecer de importar componentes
+import { useEffect, useState } from "react";
+import { IoAddCircle } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+
+import '././home.css';
 
 // importando variáveis de ambiente para requisições API
-const URL = import.meta.env.VITE_API;
+const URL = import.meta.env.VITE_URL;
 
 const Home = () => {
-// método 'async fetch API' feito com criptografia de key
-// e parâmetros de UseEffect para efetuar a requisição com o carregamento da página
-  // const [topMovies, setTopMovies] = useState([]);
+  const [allBooks, setBooks] = useState([]);
 
-  // const getBooks   = async (url) => {
-  //   const response = await fetch(url);
-  //   const data     = await response.json();
+  const   getBooks  = async (url) => {
+    const response  = await fetch(url);
+    const data      = await response.json();
 
-  //   setTopMovies(data.results);
-  // }
+    setBooks(data);
+  }
 
-  // useEffect(() => {
-  //   const viewbooksUrl = `${URL}`;
+  useEffect(() => {
+    const viewbooksUrl = `${URL}`;
 
-  //   getBooks(viewbooksUrl);
-  // }, [])
+    getBooks(viewbooksUrl);
+  }, []);
 
   return (
-    <p>Olá eu sou seu amiguinho, o Dollynho</p>
+    <main>
+      <div className="introducao">
+        <p>A sua biblioteca de livros nacionais ideal!
+          Pesquise e obtenha informações sobre diversos
+          títulos brasileiros em nosso site.</p>
+      </div>
+
+      <div className="colection-books poppins">
+        <ul>
+          <li className="create-book contaneir-book">
+          <Link to="create">
+            <button name='icon-create' id="icon-create"> <IoAddCircle fontSize={70}/> </button>
+            <h2>Adicionar Livro</h2>
+            <p>Aumente a coleção!</p>
+          </Link>
+          </li>
+
+          {allBooks.map((book, index) => (
+            <Link to={`/book/${book._id}`} key={book._id}>
+              <li className="contaneir-book">
+                <div className="contaneir-img">
+                  <img className="contaneir-img" src={`${book.cover}`}/>
+                </div>
+                <h2>{book.name}</h2>
+                <p>{book.author}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+    </main>
   );
 };
 
